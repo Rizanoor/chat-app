216e7b31cdd3a2241cfbce4f54b0d551a5e43005
rizanoor <rizanoorfauzan123@gmail.com>
2024-10-16 11:12:44 +0700

    fix: event message

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -3,17 +3,17 @@
 namespace App\Events;
 
 use App\Models\Message;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MessageSent
+class MessageSent implements ShouldBroadcastNow
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
 
     /**
      * Create a new event instance.
@@ -34,4 +34,5 @@ class MessageSent
             new PrivateChannel("chat.{$this->message->receiver_id}"),
         ];
     }
+    
 }
